b21576675c3dcb1ffad39d7041008394bdd601ba
Tom Herman <iamtomherman@gmail.com>
2025-09-12 17:16:46 +0100

    refactor(track): use explicit this.props.track property access

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -25,12 +25,11 @@ class Track extends React.Component {
   }
 
   render() {
-    const { track } = this.props;
     return (
       <div className="Track">
         <div className="Track-information">
-          <h3>{track.name}</h3>
-          <p>{track.artist} | {track.album}</p>
+          <h3>{this.props.track.name}</h3>
+          <p>{this.props.track.artist} | {this.props.track.album}</p>
         </div>
         {this.renderAction()}
       </div>
