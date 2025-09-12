9845179f5f293095377447440a4df08b1e894813
Tom Herman <iamtomherman@gmail.com>
2025-09-12 17:01:16 +0100

    feat(tracklist): render dynamic tracks passed via props

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './TrackList.css';
-// import Track from '../Track/Track';
+import Track from '../Track/Track';
 
 class TrackList extends React.Component {
   render() {
+    const { tracks = [], onAdd, onRemove, isRemoval } = this.props;
     return (
       <div className="TrackList">
-        {/* You will add a map method that renders a set of Track components  */}
-        {/* <Track /> <Track /> <Track /> */}
+        {tracks.map(t => (
+          <Track key={t.id} track={t} onAdd={onAdd} onRemove={onRemove} isRemoval={isRemoval} />
+        ))}
       </div>
     );
   }
