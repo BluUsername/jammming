bc47a8fec215d518e3d708dc24ae3c9238b8d0d0
Tom Herman <iamtomherman@gmail.com>
2025-09-12 17:18:40 +0100

    feat(playlist): pass playlist name and tracks to TrackList with editable title

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './Playlist.css';
-// import TrackList from '../TrackList/TrackList';
+import TrackList from '../TrackList/TrackList';
 
 class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    if (this.props.onNameChange) this.props.onNameChange(e.target.value);
+  }
+
   render() {
+    const { name = 'New Playlist', tracks = [], onRemove, onSave } = this.props;
     return (
       <div className="Playlist">
-        <input defaultValue={'New Playlist'} />
-        {/* <TrackList /> */}
-        <button className="Playlist-save">SAVE TO SPOTIFY</button>
+        <input defaultValue={name} onChange={this.handleNameChange} />
+        <TrackList tracks={tracks} onRemove={onRemove} isRemoval={true} />
+        <button className="Playlist-save" onClick={onSave}>SAVE TO SPOTIFY</button>
       </div>
     );
   }
