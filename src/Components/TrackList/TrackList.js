8e6d18fcade704d6214131cd65861b2c4d59a3d6
Tom Herman <iamtomherman@gmail.com>
2025-09-12 16:50:25 +0100

    style(playlist): apply provided layout styles\nfeat(tracklist): scaffold class-based TrackList with placeholder

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './TrackList.css';
-import Track from '../Track/Track';
+// import Track from '../Track/Track';
 
-function TrackList({ tracks, onAdd, onRemove, isRemoval }) {
-  return (
-    <div className="TrackList">
-      {tracks.map(track => (
-        <Track key={track.id} track={track} onAdd={onAdd} onRemove={onRemove} isRemoval={isRemoval} />
-      ))}
-    </div>
-  );
+class TrackList extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+        {/* You will add a map method that renders a set of Track components  */}
+        {/* <Track /> <Track /> <Track /> */}
+      </div>
+    );
+  }
 }
+
 export default TrackList;
