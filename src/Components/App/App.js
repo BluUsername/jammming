416cd0b4f94004c0b835029dbbdfcc1b823d2a65
Tom Herman <iamtomherman@gmail.com>
2025-09-12 21:02:29 +0100

    refactor(app): remove mock data and legacy search logic

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,51 +5,19 @@ import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 import Spotify from '../../util/Spotify';
 
-// Temporary mock search data (will be replaced with Spotify API)
-const mockSearch = (term) => {
-	const base = [
-		{ id: '1', name: 'Song One', artist: 'Artist A', album: 'Album X', uri: 'spotify:track:1' },
-		{ id: '2', name: 'Song Two', artist: 'Artist B', album: 'Album Y', uri: 'spotify:track:2' },
-		{ id: '3', name: 'Another Track', artist: 'Artist A', album: 'Album X', uri: 'spotify:track:3' },
-		{ id: '4', name: 'Last Song', artist: 'Artist C', album: 'Album Z', uri: 'spotify:track:4' }
-	];
-	const lower = term.toLowerCase();
-	return base.filter(t =>
-		t.name.toLowerCase().includes(lower) ||
-		t.artist.toLowerCase().includes(lower) ||
-		t.album.toLowerCase().includes(lower)
-	);
-};
-
 class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			// Hard-coded initial search results (will be replaced by real Spotify data later)
-			searchResults: [
-				{ id: '101', name: 'Sample Track One', artist: 'Sample Artist A', album: 'Sample Album X', uri: 'spotify:track:101' },
-				{ id: '102', name: 'Sample Track Two', artist: 'Sample Artist B', album: 'Sample Album Y', uri: 'spotify:track:102' },
-				{ id: '103', name: 'Example Song', artist: 'Demo Artist', album: 'Collection Z', uri: 'spotify:track:103' }
-			],
-			playlistName: 'My Starter Playlist',
-			// Hard-coded initial playlist tracks
-			playlistTracks: [
-				{ id: '201', name: 'Starter Song A', artist: 'Playlist Artist 1', album: 'Playlist Album 1', uri: 'spotify:track:201' },
-				{ id: '202', name: 'Starter Song B', artist: 'Playlist Artist 2', album: 'Playlist Album 2', uri: 'spotify:track:202' }
-			]
+			searchResults: [],
+			playlistName: 'New Playlist',
+			playlistTracks: []
 		};
-		this.handleSearch = this.handleSearch.bind(this);
 		this.search = this.search.bind(this);
 		this.addTrack = this.addTrack.bind(this);
 		this.removeTrack = this.removeTrack.bind(this);
 		this.updatePlaylistName = this.updatePlaylistName.bind(this);
 		this.savePlaylist = this.savePlaylist.bind(this);
-	}
-
-	handleSearch(term) {
-		const results = mockSearch(term);
-		const playlistIds = new Set(this.state.playlistTracks.map(t => t.id));
-		this.setState({ searchResults: results.filter(r => !playlistIds.has(r.id)) });
 	}
 
 	// Search via Spotify API utility
